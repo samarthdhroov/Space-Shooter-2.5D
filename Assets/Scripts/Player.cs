0d16929791a1a08a5fded8943fb26c74f03d4f8a
LAPTOP-PK9AJCBI\samarthdhroov <samarth.dhroov@hotmail.com>
2021-04-29 11:19:08 +0530

    Added player script and serailized speed variable.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] // Attribute that serailizes the data.
+                     // It will allow data to be read as well as override from the inspector.
+                     // However, other game objects or scripts would not be able to access it. 
+    private float _speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
+        transform.position = new Vector3(0, -4.74f, 0);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+
     }
 }
